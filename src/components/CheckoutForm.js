aa56ec77a4483710e9569158853c728c4f113e1a
tomineodegard <tomi0105@stud.kea.dk>
2022-05-13 21:03:11 +0200

    checkout form

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,8 +1,25 @@
+import {useRef} from "react"
+
 export default function CheckoutForm() {
+    const formElement = useRef(null);
+
+    function submitted (e) {
+        e.preventDefault();
+        console.log(formElement.current.elements.fullname.value);
+        console.log(formElement.current.elements.email.value);
+    }
+
     return (
-        <div>
-            <form>
-                <input />
+        <div className="checkout_form">
+            <h5>* Please enter your contact info to complete the checkout</h5>
+            <form ref={formElement} onSubmit={submitted}>
+                
+                <label htmlFor="fullname">Full name</label>
+                <input  required type="text" name="fullname" id="fullname" />
+
+                <label htmlFor="email">Email</label>
+                <input required type="text" name="email" id="email" />
+                <button>Proceed to checkout</button>
             </form>
         </div>
     );
