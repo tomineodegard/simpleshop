df15853a3275df598a042456bdd1014c944761f2
tomineodegard <tomi0105@stud.kea.dk>
2022-05-13 17:31:01 +0200

    importing stylesheet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Nav from "./components/Nav";
 import ProductList from "./components/ProductList";
 import BasketContainer from "./components/BasketContainer";
+import styles from "./App.css";
 
 function App() {
   return (
